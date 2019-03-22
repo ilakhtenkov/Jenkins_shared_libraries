6f909fb5119c7d8ee00c860558e2effa8c0a4b2e
Igor Lakhtenkov <Igor_Lakhtenkov@epam.com>
2019-03-22 10:24:58 +0000

    TestClass added

--- src/org/test/TestClass.groovy
+++ src/org/test/TestClass.groovy
@@ -1,12 +1,13 @@
 class TestClass implements Serializable {
 
     private pipelineSteps
+    private static final TEST_STRING="Hello from shared lib"
 
     TestClass(pipelineSteps) {
         this.pipelineSteps = pipelineSteps
     }
 
     public def testMethod() {
-        
+        return TEST_STRING
     }
 }
