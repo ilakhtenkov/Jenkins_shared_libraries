349230ef5415def71b170cb874b752b7bc267900
Igor Lakhtenkov <Igor_Lakhtenkov@epam.com>
2019-04-08 13:55:23 +0000

    test txt

--- src/org/test/BitBucket.groovy
+++ src/org/test/BitBucket.groovy
@@ -5,5 +5,25 @@ import groovyx.net.http.HTTPBuilder
 import groovyx.net.http.Method
 
 class BitBucket implements Serializable {
+    
     private static final Integer BITBUCKET_LIMIT = 100
+    private String serverUri, projectUri, project, repoName, bbUser, bbPasswd, repoUri
+    private pipelineSteps 
+   
+    BitBucket(pipelineSteps, String bbUser,
+          String bbPasswd,
+          String serverUri="",
+          String project="",
+          String repoName="") {
+    this.pipelineSteps = pipelineSteps
+    this.serverUri = serverUri
+    this.project = project
+    this.repoName = repoName
+    this.bbUser = bbUser
+    this.bbPasswd = bbPasswd
+    this.projectUri = "rest/api/1.0/projects/${project}"
+    this.repoUri = "rest/api/1.0/projects/${project}/repos/${repoName}"
+    
+    String getLog() {
+        
 }
